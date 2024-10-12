92be8376cea79cf88ea89240200bb2f2e7e81af3
NeutronStarT <>
2024-10-12 21:34:23 +0800

    [FEAT&FIX] 定制文件名+修正编辑过的推文tweet_msecs无法获取的异常
    代码很脏很脏= =，可能以后会重构吧（懒）
    
    # Conflicts:
    #	main.py

--- cache_gen.py
+++ cache_gen.py
@@ -1,10 +1,11 @@
 import os
-import pickle
+import threading
 
 class cache_gen():
 
     def __init__(self, save_path) -> None:
         self.cache_path = save_path + os.sep + "cache_data_v2.log"
+        self.lock = threading.Lock()
 
         if os.path.exists(self.cache_path):
             with open(self.cache_path, 'r') as f:
@@ -18,14 +19,17 @@ class cache_gen():
 
     def is_present(self, element):
         if element in self.cache_data:
+            print(f'{element} is existed')
             return False
         else:
             self.add(element)
             return True
     
     def save_cache(self, element):
-        if element not in self.cache_data:
-            self.cache_data.add(element)
-        with open(self.cache_path, 'a') as f:
-            f.write(element + '\n')
+        self.lock.acquire()
+        try:
+            with open(self.cache_path, 'a') as f:
+                f.write(element + '\n')
+        finally:
+            self.lock.release()
 
